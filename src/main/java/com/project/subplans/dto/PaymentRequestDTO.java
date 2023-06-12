eef1d1a385c6a2337fb92063f21dcf1b29ce15cf
Mad Man <113254019+SBimsara@users.noreply.github.com>
2023-06-12 16:02:04 +0530

    created payment controller, dto and service

---
+++ src/main/java/com/project/subplans/dto/PaymentRequestDTO.java
@@ -0,0 +1,16 @@
+package com.project.subplans.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class PaymentRequestDTO {
+    private long amount;
+}
