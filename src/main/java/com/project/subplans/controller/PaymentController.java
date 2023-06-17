61a5d60e801fa9288e03866c79f7ab2b28bf0895
Mad Man <113254019+SBimsara@users.noreply.github.com>
2023-06-17 23:36:05 +0530

    deleted files

--- src/main/java/com/project/subplans/controller/PaymentController.java
+++ src/main/java/com/project/subplans/controller/PaymentController.java
@@ -1,43 +1,4 @@
 
-//
-//
-//import com.project.subplans.dto.ChargeRequestDTO;
-//import com.project.subplans.dto.ChargeResponseDTO;
-//import com.project.subplans.mapper.ChargeMapper;
-//import com.project.subplans.service.PaymentService;
-//import com.project.subplans.util.StatusList;
-//import com.stripe.exception.StripeException;
-//import com.stripe.model.Charge;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequiredArgsConstructor
-//@RequestMapping(value = "api/v1/payment")
-//@CrossOrigin(origins="http://192.168.1.4:19000")
-//public class PaymentController {
-//
-//    @Autowired
-//    private PaymentService paymentService;
-//    @Autowired
-//    private final ChargeMapper chargeMapper;
-//
-//
-//    @PostMapping(value = "/charge")
-//    public String charge(@RequestBody ChargeRequestDTO chargeRequestDTO) throws StripeException {
-//        String clientSecret = paymentService.paymentProcess(chargeRequestDTO);
-////        return chargeMapper.toChargeResponse(charge);
-//        return clientSecret;
-//    }
-//
-//
-//
-//}
 
 package com.project.subplans.controller;
 
