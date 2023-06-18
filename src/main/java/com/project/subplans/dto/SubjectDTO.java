6cbb7bcfe9e53960c1e6bb12ccee8b8fd27357f1
Mad Man <113254019+SBimsara@users.noreply.github.com>
2023-06-18 19:47:41 +0530

    Update SubjectDTO.java

--- src/main/java/com/project/subplans/dto/SubjectDTO.java
+++ src/main/java/com/project/subplans/dto/SubjectDTO.java
@@ -1,18 +1,17 @@
 package com.project.subplans.dto;
 
 import com.project.subplans.entity.Lesson;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.Set;
 
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
+@Getter
+@Setter
 public class SubjectDTO {
     private int id;
     private String subjectname;
     private String grade;
-    private Set<Lesson> lessons;
+    private Set<Plan> plans;
 }
