712d1a7e177228b7adf8c744df2ee04e37f6eb57
Mad Man <113254019+SBimsara@users.noreply.github.com>
2023-06-18 19:49:33 +0530

    Update Plan.java

--- src/main/java/com/project/subplans/entity/Plan.java
+++ src/main/java/com/project/subplans/entity/Plan.java
@@ -1,25 +1,37 @@
 package com.project.subplans.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
+@Getter
+@Setter
+
 @Table(name = "Plan")
 public class Plan {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int pid;
+    private int id;
     private String name;
     private float price;
     private float discount;
 
-
-
+    @JsonIgnoreProperties("plans")
+    @ManyToMany
+    @JoinTable(name = "Subscriptioned Subjects",
+    joinColumns = {
+            @JoinColumn(name = "plan_id",referencedColumnName = "id")
+    },
+    inverseJoinColumns = {
+            @JoinColumn(name = "subject_id",referencedColumnName = "id")
+    }
+    )
+    private Set<Subject> subjects;
 }
