3812338dce97fa2b4cf089a6d09895920c1a67b0
Mad Man <113254019+SBimsara@users.noreply.github.com>
2023-06-18 19:50:45 +0530

    Update Subject.java

--- src/main/java/com/project/subplans/entity/Subject.java
+++ src/main/java/com/project/subplans/entity/Subject.java
@@ -1,17 +1,19 @@
 package com.project.subplans.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+import java.util.HashSet;
 import java.util.Set;
 
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
+@Setter
+@Getter
+
 @Table(name = "Subject")
 public class Subject {
     @Id
@@ -20,7 +22,20 @@ public class Subject {
     private String subjectname;
     private String grade;
 
-    @OneToMany(mappedBy = "subject", cascade = CascadeType.ALL)
-    private Set<Lesson> lessons;
+
+    @JsonIgnoreProperties("subjects")
+    @ManyToMany(mappedBy = "subjects")
+    private Set<Plan> plans ;
+
+    public void addPlan(Plan plan) {
+        plans.add(plan);
+        plan.getSubjects().add(this);
+    }
+
+    public void removePlan(Plan plan) {
+        plans.remove(plan);
+        plan.getSubjects().remove(this);
+    }
+
 
 }
