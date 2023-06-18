51255305e7c69243180ae2c0d2f3ee35520ef0d4
Mad Man <113254019+SBimsara@users.noreply.github.com>
2023-06-18 18:55:40 +0530

    Revert "removed plan files"
    This reverts commit ca7c9b52d2b4285ca6701476b42cad801ecc406f.

---
+++ src/main/java/com/project/subplans/dto/PlanDTO.java
@@ -0,0 +1,16 @@
+package com.project.subplans.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class PlanDTO {
+    private int id;
+    private String name;
+    private float price;
+    private float discount;
+
+}
---
+++ src/main/java/com/project/subplans/entity/Plan.java
@@ -0,0 +1,25 @@
+package com.project.subplans.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "Plan")
+public class Plan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int pid;
+    private String name;
+    private float price;
+    private float discount;
+
+
+
+}
